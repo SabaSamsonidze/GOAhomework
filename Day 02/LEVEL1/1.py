0b29307cb39c89124f15aeb882f8794f4ee8b24f
SabaSamsonidze <ssamsonidze2@gmail.com>
2025-02-15 18:51:26 +0400

    commit

--- Day 02/LEVEL1/1.py
+++ Day 02/LEVEL1/1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 # data type
 
@@ -31,38 +30,4 @@ False
 "hello" * 2 # 
 "hello" / 2 # error
 "hello" + 120 # error
-=======
-
-# data type
-
-# string
-# integer
-# float
-
-
-
-
-# control flow, 3.  syntax error, logical error
-
-
-"hello lasha" # string
-11 # integer
-3.4 # float
-True
-False
-10>5
-
-# case sensitive, case insensitive
-
-"hello " + "lasha" # "hello lasha"
-11 + 11 # 22
-"11" + " 11" # 1111
-"hello" - "lasha" # error
-11 - 11 # 0
-"hello" * "lasha" # error
-11 * 11 # 121 
-"hello" * 2 # 
-"hello" / 2 # error
-"hello" + 120 # error
->>>>>>> fa2c142a228ef35090397ac6969406c8b04c89f1
 "hello" - 120 # error
