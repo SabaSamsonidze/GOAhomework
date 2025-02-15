27bfad820e788b363033cb0ef4abd91760d7edd1
SabaSamsonidze <ssamsonidze2@gmail.com>
2025-02-15 18:37:36 +0400

    commit

---
+++ Day 02/LEVEL1/1.py
@@ -0,0 +1,33 @@
+
+# data type
+
+# string
+# integer
+# float
+
+
+
+
+# control flow, 3.  syntax error, logical error
+
+
+"hello lasha" # string
+11 # integer
+3.4 # float
+True
+False
+10>5
+
+# case sensitive, case insensitive
+
+"hello " + "lasha" # "hello lasha"
+11 + 11 # 22
+"11" + " 11" # 1111
+"hello" - "lasha" # error
+11 - 11 # 0
+"hello" * "lasha" # error
+11 * 11 # 121 
+"hello" * 2 # 
+"hello" / 2 # error
+"hello" + 120 # error
+"hello" - 120 # error
