2d89bee233e01d30d337cc1e6dd792faa80df183
SabaSamsonidze <ssamsonidze2@gmail.com>
2025-02-18 22:04:58 +0400

    commit

---
+++ Day 02/classwork.py
@@ -0,0 +1,24 @@
+num1 = int(input("First number"))
+num2 = int(input("Second number"))
+num3 = int(input("Third number"))
+
+print(num1+num2+num3)
+
+name = input("Enter username:")
+
+
+num1 = (input("enter first number"))
+num2 = (input("enter second number"))
+print(num1>num2)
+
+word = input("enter word")
+n = int(input("enter number"))
+print(word * n)
+
+num4 = input("Enter first number: ")
+
+num5 =input("Enter second number: ")
+
+combined_number = int(num4 + num5)
+
+print(combined_number + 1)
