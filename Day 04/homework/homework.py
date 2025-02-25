4fc1e3a0b90f9464559561599cffe8e41d7b7f67
SabaSamsonidze <ssamsonidze2@gmail.com>
2025-02-25 13:23:03 +0400

    commit

---
+++ Day 04/homework/homework.py
@@ -0,0 +1,44 @@
+# 1) დავალება
+age = int(input("Enter your age:"))
+
+if age >= 6:
+    print("You are in school")
+
+elif age > 2:
+    print("You are in kindergarden")
+
+elif age < 2:
+    print("You are a newborn")
+
+else:
+    print("You are employed")
+
+
+
+# 2) დავალება
+
+
+information = input("Enter which programming academy do you go to?: ")
+
+if information == "Goa":
+    print("U r a real chad!")
+else:
+    print("Join Goa, become a chad!")
+
+
+# 3) დავალება
+
+score = int(input("Enter your score: "))
+
+if score > 90:
+    print("A")
+elif score > 75:
+    print("B")
+elif score > 60:
+    print("C")
+elif score > 50:
+    print("D")
+elif score > 40:
+    print("E")
+elif score > 30:
+    print("F")
