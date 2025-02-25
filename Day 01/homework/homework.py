ed5ea22e86873318b362c23078643a1507d73df7
SabaSamsonidze <ssamsonidze2@gmail.com>
2025-02-25 15:52:07 +0400

    commit

--- Day 01/homework/homework.py
+++ Day 01/homework/homework.py
@@ -1,1 +1,9 @@
-print()
+#პირობა უნდა შექმნა სახელი გვარი ასაკი სიმაღლე საყვარელი ფერი
+name = "saba"  # string/str
+surname= "samsonidze" # string/str
+age = 16 # integer/int
+height = 1.95 # float
+favorite_color = "black" # string/str
+i_love_basketball = True #boolean/bool
+
+print(name + " " + surname  + " " + str(age) + " " + str(height) + " " +  favorite_color + " " + str(i_love_basketball) )
