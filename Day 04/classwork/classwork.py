d0d88eae7fb1f07b0850287d9f2cc54d8f780f5d
SabaSamsonidze <ssamsonidze2@gmail.com>
2025-02-25 14:43:59 +0400

    commit

--- Day 04/classwork/classwork.py
+++ Day 04/classwork/classwork.py
@@ -1,3 +1,50 @@
+# პირველი დავალება
+num1 = int(input("შეიყვანეთ რიცხვი: "))
+
+if num1 % 2 == 0:
+    print("ლუწი")
+else:
+    print("კენტი")
+
+ # მეორე დავალება
+
+number = int(input("Enter a number: "))
+
+if number > 0:
+    res = 1
+elif number < 0:
+    res = -1
+else:
+    res = 0
+
+print(res)
+
+ # მესამე დავალება
+
+num3 = int(input("Enter first number: "))
+num4 = int(input("Enter second number: "))
+
+if num3 > num4:
+    print(num3)
+elif num4 > num3:
+  print(num4)
+
+ # მეოთხე დავალება
+
+number = int(input("Enter a number: "))
+
+if number % 5 == 0:
+    print(number)
+else:
+    print(0)
+
+# მეხუთე დავალება
+num5 = int(input("Enter first number: "))
+
+if num5 % 100 == 0:
+    print("ახალი საუკუნე")
+else:
+    print("ძველი საუკუნე")
 
 
 
